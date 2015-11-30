be24184defb89403d3dab94f38d3897035eb7570
Michael Beasley <youvegotmoxie@gmail.com>
2015-11-29 20:47:00 -0500

    Create get-deps-fedora-dnf.sh

---
+++ tools/get-deps-fedora-dnf.sh
@@ -0,0 +1,33 @@
+# Packages we need under Fedora
+# yum provides
+# yum list
+dnf install \
+	binutils-devel \
+	bison \
+	crash \
+	elfutils-devel \
+	elfutils-libelf-devel \
+	flex \
+	gcc \
+	glibc-devel \
+	glibc-devel.i686 \
+	kernel-devel \
+	libdwarf \
+	libdwarf-devel \
+	libgcc.i686 \
+	make \
+	perl \
+	zlib-devel
+
+# Optional:
+#  openssh-clients
+
+#binutils-devel
+#elfutils-libelf-devel
+#kernel-devel
+#libdwarf
+#libdwarf-devel
+#zlib-devel
+# yumdownloader --source kernel
+# yum install rpmdevtools yum-utils
+# http://fedoraproject.org/wiki/Building_a_custom_kernel
